d8819d1566191682522c647c84a4701c408a3d53
DESKTOP-VNHC8D8\KGA_015 <zxc7187@naver.com>
2022-07-13 15:05:01 +0900

    아이템 상호작용 진행중

---
+++ Assets/Game/Scripts/Item/ItemInterAction(YJ)/InteractionNatureObject.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionNatureObject: MonoBehaviour, IGlow, IInteraction
+{
+    private Renderer objectRenderer;
+    private Material material;
+    private bool isLoop = false;
+    public string[] itemList = new string[0];
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        material = objectRenderer.material;
+    }
+    private void Update()
+    { 
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 5f, 1 << LayerMask.NameToLayer("Player"));
+        if(colliders.Length > 0)
+        {
+            StartCoroutine(Glow());
+        }
+        else
+        {
+            material.SetColor("_EmissionColor", Color.yellow * 0);
+            StopAllCoroutines();
+            isLoop = false;
+        }
+    }
+
+
+    public IEnumerator Glow()
+    {
+        if (!isLoop)
+        {
+            Debug.Log("루프 시작");
+            isLoop = true;
+            for (float i = 0; i < 1;)
+            {
+                material.SetColor("_EmissionColor", Color.yellow * i);
+                yield return new WaitForSeconds(0.06f);
+                i += 0.1f;
+            }
+            yield return new WaitForSeconds(0.1f);
+            for (float i = 1; i > 0;)
+            {
+
+                material.SetColor("_EmissionColor", Color.yellow * i);
+                yield return new WaitForSeconds(0.1f);
+                i -= 0.1f;
+            }
+            yield return new WaitForSeconds(0.4f);
+            isLoop = false;
+            Debug.Log("루프 끝");
+        }
+    }
+
+    public void Interaction()
+    {
+
+    }
+
+}
---
+++ Assets/Game/Scripts/Utility/InterfaceFunction.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHit
+{
+    public void Hit();
+}
+
+//상호작용 하는거
+public interface IInteraction
+{
+    public void Interaction();
+}
+
+//보스 유실물은 빛나지 않으니까 IGlow 따로 나눴습니다
+public interface IGlow
+{
+    public IEnumerator Glow();
+}
